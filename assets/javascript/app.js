54f19f45d7762bad3340b55bdeed1b7ff5d7621e
Weston Carpenter <wc.cc96@gmail.com>
2019-09-05 16:27:45 -0700

    Added required directories and files, generated sample questions with answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+Question1 = {
+    question: "What is my first name",
+    right: "Weston",
+    wrong1: "Bob",
+    wrong2: "Joe",
+    wrong3: "Chris",
+}
+Question3 = {
+    question: "What is my last name",
+    right: "Cedrick",
+    wrong1: "James",
+    wrong2: "William",
+    wrong3: "David",
+}
+Question3 = {
+    question: "What is my last name",
+    right: "Carpenter",
+    wrong1: "Doe",
+    wrong2: "Smith",
+    wrong3: "Miller",
+}
+
+var questions=[Question1,Question2,Question3]
+
