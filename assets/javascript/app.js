0860be42bd05543537f42da82034c18724a75330
Weston Carpenter <wc.cc96@gmail.com>
2019-09-05 23:10:05 -0700

    Game now logs all questions (shuffled) with their respective answers (shuffled) and the correct answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,62 @@
 Question1 = {
-    question: "What is my first name",
-    right: "Weston",
-    wrong1: "Bob",
-    wrong2: "Joe",
-    wrong3: "Chris",
+    ask: "What is my first name?",
+    answers: ["Weston", "Bob", "Joe", "Chris"],
+    rightAnswer: "Weston"
+}
+Question2 = {
+    ask: "What is my middle name?",
+    answers: ["Cedrick", "James", "William", "David"],
+    rightAnswer: "Cedrick"
 }
 Question3 = {
-    question: "What is my last name",
-    right: "Cedrick",
-    wrong1: "James",
-    wrong2: "William",
-    wrong3: "David",
-}
-Question3 = {
-    question: "What is my last name",
-    right: "Carpenter",
-    wrong1: "Doe",
-    wrong2: "Smith",
-    wrong3: "Miller",
+    ask: "What is my last name?",
+    answers: ["Carpenter", "Doe", "Smith", "Miller"],
+    rightAnswer: "Carpenter"
 }
 
-var questions=[Question1,Question2,Question3]
+var questions = [Question1, Question2, Question3]
 
+
+shuffle(questions);
+
+questions.forEach(question => {
+    console.log(question.ask);
+    shuffle(question.answers);
+    question.answers.forEach(answer => {
+        console.log(answer);
+    });
+    console.log('The right answer is ' + question.rightAnswer);
+});
+
+
+
+
+
+
+
+// Fisher-Yates Shuffle
+function shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+
+    return array;
+}
+
+
+
+
+
+// for (let i = 0; i < questions.length; i++) {
+//     displayQuestion();  
+// }
